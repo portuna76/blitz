628404f715a638780b134755748434e3091520f9
portuna85 <portuna85@gmail.com>
2022-08-23 18:36:58 +0900

    Update

--- deploy/develop/script/deploy_server.sh
+++ deploy/develop/script/deploy_server.sh
@@ -1,37 +1,47 @@
+#!/bin/bash
+
 REPOSITORY=/var/lib/jenkins/workspace
 PROJECT_NAME=blitz
-BUILD_PATH=$(ls -tr ${REPOSITORY}/${PROJECT_NAME}/build/lib/*.jar | tail -1)
-JAR_NAME=$(basename $BUILD_PATH)
-echo "> build file: $JAR_NAME"
 
-echo "> copy build file"
-DEPLOY_PATH=${REPOSITORY}/deploy/
-if [ ! -d $DEPLOY_PATH ]; then
-  mkdir $DEPLOY_PATH
-fi
+cd $REPOSITORY/$PROJECT_NAME/
 
-cp $BUILD_PATH $DEPLOY_PATH
+echo "> Git pull"
 
-echo "> change file name"
-CP_JAR_PATH=$DEPLOY_PATH$JAR_NAME
-APPLICATION_JAR_NAME=application-deploy.jar
-APPLICATION_JAR=$DEPLOY_PATH$APPLICATION_JAR_NAME
+Git pull
 
-echo "> create link"
-ln -Tfs $CP_JAR_PATH $APPLICATION_JAR
+echo "> 프로젝트 Build 시작"
 
-echo "> Check application PID."
-CURRENT_PID=$(pgrep -f -n $APPLICATION_JAR_NAME)
-echo "$CURRENT_PID"
+./gradlew build
 
-if [ -z $CURRENT_PID ];
-  then
-    echo "> No running applications found."
+echo "> workspace 프로젝트로 이동"
+
+cd $REPOSITORY
+
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 PID 확인"
+
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+
+echo "> 현재 구동중인 애플리케이션 PID: $CURRENT_PID"
+
+if [ -z "CURRENT_PID" ]; then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-	echo "> kill -9 $CURRENT_PID"
-	kill -9 $CURRENT_PID
-	sleep 10
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
 fi
 
-echo "> Run application."
-nohup java -jar -Dspring.profiles.active=local $APPLICATION_JAR > /dev/null 2> /dev/null < /dev/null &
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/var/lib/jenkins/application-oauth.properties,/var/lib/jenkins/application-real-db.properties \
+  -Dspring.profiles.active=real \
+  $REPOSITORY/$JAR_NAME 2>&1 &
