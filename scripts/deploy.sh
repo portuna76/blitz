a1e5005d8ea64747f614076a4e9d4945bf91cb5b
portuna85 <portuna85@gmail.com>
2022-08-24 15:01:51 +0900

    Deploy Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,29 +7,29 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 구동중인 애플리케이션 PID 확인"
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl blitz | grep jar | awk '{print $1}')
 
-echo "현재 구동중인 애플리케이션 PID : $CURRENT_PID"
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 20
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 20
 fi
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR Name : $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행 권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가"
 
-sudo chmod +x $JAR_NAME
+chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
