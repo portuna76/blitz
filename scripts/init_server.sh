a93cdccca0624e937672961d83703a13aed65708
portuna85 <portuna85@gmail.com>
2022-08-25 16:24:26 +0900

    Deploy Update

--- scripts/init_server.sh
+++ scripts/init_server.sh
@@ -1,19 +1,34 @@
-echo "> 현재 실행중인 project pid 확인"
+#!/bin/bash
 
-CURRENT_PID=$(ps -ef | grep java | grep blitz | grep -v nohup | awk '{print $2}')
+REPOSITORY=/home/ec2-user/blitz/scripts
 
-echo "$CURRENT_PID"
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-if [ -z ${CURRENT_PID} ] ;then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+CURRENT_PID=$(pgrep -fl blitz | grep jar | awk '{print $1}')
+
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> sudo kill -9 $CURRENT_PID"
-  sudo kill -9 $CURRENT_PID
-  sleep 10
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
-echo "> blitz 배포"
+echo "> 새 어플리케이션 배포"
 
-JAR_PATH=$(ls -t /home/ubuntu/blitz/deploy/*.jar | head -1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-sudo nohup java -jar -DServer.port=80 -Dspring.profiles.active=dev ${JAR_PATH} >> /home/ubuntu/blitz/logs/jujeol.log &
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/blitz/application-oauth.properties,/home/ec2-user/blitz/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
